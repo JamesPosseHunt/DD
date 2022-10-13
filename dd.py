0b0f8b2f165c8f397f2010ab72d8e011286a0bff
JamesPOliver <jamesjordanpo@gmail.com>
2022-10-13 21:55:22 +0100

    Tidied up

--- dd.py
+++ dd.py
@@ -34,24 +34,23 @@ def menu():
 
 window = Tk()
 window.title("DD")
-window.geometry('1920x1080')
 
-b4 = Button(window, text="D4", width=5, height=10, activebackground='blue', bg='grey', command=d4)
-b4.place(x=200,y=30)
+b4 = Button(window, text="D4", width=10, height=5, activebackground='blue', bg='grey', command=d4)
+b4.pack()
 
-b6 = Button(window, text="D6", width=5, height=10, activebackground='blue', bg='grey', command=d6)
-b6.place(x=250,y=30)
+b6 = Button(window, text="D6", width=10, height=5, activebackground='blue', bg='grey', command=d6)
+b6.pack()
 
-b8 = Button(window, text="D8", width=5, height=10, activebackground='blue', bg='grey', command=d8)
-b8.place(x=300, y=30)
+b8 = Button(window, text="D8", width=10, height=5, activebackground='blue', bg='grey', command=d8)
+b8.pack()
 
-b10 = Button(window, text="D10", width=5, height=10, activebackground='blue', bg='grey', command=d10)
-b10.place(x=350, y=30)
+b10 = Button(window, text="D10", width=10, height=5, activebackground='blue', bg='grey', command=d10)
+b10.pack()
 
-b12 = Button(window, text="D12", width=5, height=10, activebackground='blue', bg='grey', command=d12)
-b12.place(x=400, y=30)
+b12 = Button(window, text="D12", width=10, height=5, activebackground='blue', bg='grey', command=d12)
+b12.pack()
 
-b20 = Button(window, text="D20", width=5, height=10, activebackground='blue', bg='grey', command=d20)
-b20.place(x=450, y=30)
+b20 = Button(window, text="D20", width=10, height=5, activebackground='blue', bg='grey', command=d20)
+b20.pack()
 
 window.mainloop()

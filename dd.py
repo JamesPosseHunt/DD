cc326b519c2d794f877d99bcc994d76ebcfa15e0
JamesPOliver <jamesjordanpo@gmail.com>
2022-10-21 15:12:09 +0100

    Set Dice to open in it's own window

--- dd.py
+++ dd.py
@@ -32,25 +32,31 @@ def menu():
     print("D10 (Press 10), D12 (Press 12), D20 (Press 20)")
     print("Exit (Press 0)")
 
+def dice():
+    new_win = Tk()
+    new_win.title("Dice")
+    b4 = Button(new_win, text="D4", width=10, height=5, activebackground='blue', bg='grey', command=d4)
+    b4.pack()
+
+    b6 = Button(new_win, text="D6", width=10, height=5, activebackground='blue', bg='grey', command=d6)
+    b6.pack()
+
+    b8 = Button(new_win, text="D8", width=10, height=5, activebackground='blue', bg='grey', command=d8)
+    b8.pack()
+
+    b10 = Button(new_win, text="D10", width=10, height=5, activebackground='blue', bg='grey', command=d10)
+    b10.pack()
+
+    b12 = Button(new_win, text="D12", width=10, height=5, activebackground='blue', bg='grey', command=d12)
+    b12.pack()
+
+    b20 = Button(new_win, text="D20", width=10, height=5, activebackground='blue', bg='grey', command=d20)
+    b20.pack()
+
 window = Tk()
 window.title("DD")
 
-b4 = Button(window, text="D4", width=10, height=5, activebackground='blue', bg='grey', command=d4)
-b4.pack()
-
-b6 = Button(window, text="D6", width=10, height=5, activebackground='blue', bg='grey', command=d6)
-b6.pack()
-
-b8 = Button(window, text="D8", width=10, height=5, activebackground='blue', bg='grey', command=d8)
-b8.pack()
-
-b10 = Button(window, text="D10", width=10, height=5, activebackground='blue', bg='grey', command=d10)
-b10.pack()
-
-b12 = Button(window, text="D12", width=10, height=5, activebackground='blue', bg='grey', command=d12)
-b12.pack()
-
-b20 = Button(window, text="D20", width=10, height=5, activebackground='blue', bg='grey', command=d20)
-b20.pack()
+dice_btn = Button(window, text="Dice", width=10, height=5, activebackground='blue', bg='grey', command=dice)
+dice_btn.pack()
 
 window.mainloop()

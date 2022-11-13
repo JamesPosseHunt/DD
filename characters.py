3766c48621686b2fcf00ae046670922d59ce83f0
JamesPOliver <jamesjordanpo@gmail.com>
2022-11-13 01:43:56 +0000

    Added save character functionality, started load character functionality

--- characters.py
+++ characters.py
@@ -10,6 +10,7 @@ class Race:
         self.speed = speed
         self.precision = precision
         self.shields = shields
+        self.tech = 1
 
 
 class Character:
@@ -20,10 +21,87 @@ class Character:
         self.race = race
         self.background = background
         self.current_loadout = current_loadout
+        if self.race == "Spartan":
+            self.race = spartan
+        elif self.race == "Human":
+            self.race = human
+        elif self.race == "Elite":
+            self.race = elite
+        elif self.race == "Brute":
+            self.race = brute
+        elif self.race == "Grunt":
+            self.race = grunt
+        elif self.race == "Jackal":
+            self.race = jackal
+        elif self.race == "Hunter":
+            self.race = hunter
+        if self.current_loadout == "Air Assault":
+            self.current_loadout = air_assault
     def cqc(self):
-        if self.background == cqc:
+        if self.background == "cqc":
             self.race.strength += random.randint(0, 6)
-        
+
+    def engineer(self):
+        if self.background == "engineer":
+            self.race.tech += random.randint(0, 6)
+
+    def sharpshooter(self):
+        if self.background == "sharpshooter":
+            self.race.precision += random.randint(0, 6)
+    
+    def demolitions(self):
+        if self.background == "demolitions":
+            pass
+    
+    def assault(self):
+        if self.background == "assault":
+            pass
+    
+    def recon(self):
+        if self.background == "recon":
+            pass
+    
+    def ODST(self):
+        if self.background == "ODST":
+            self.race.health += random.randint(0, 4)
+            self.race.strength += random.randint(0, 4)
+            self.race.speed += random.randint(0, 4)
+            self.race.precision += random.randint(0, 4)
+    
+    def switch_weapon(self):
+        if self.current_loadout.current_weapon == self.current_loadout.primary:
+            self.current_loadout.current_weapon = self.current_loadout.secondary
+        else:
+            self.current_loadout.current_weapon == self.current_loadout.primary
+    
+    def show_character(self):
+        print("Name: ", self.name)
+        print("Race: ", self.race)
+    
+    def save_char(character):
+        file = character.name + ".ddchar"
+        with open(file, "w") as char_file:
+            char_file.write(character.name + "\n")
+            char_file.write(str(character.level) + "\n")
+            char_file.write(character.race.name + "\n")
+            char_file.write(character.current_loadout.name + "\n")
+
+class Enemy(Character):
+
+    def __init__(self, name, health, attacks):
+        self.name = name
+        self.health = health
+        self.attacks = attacks
+
+def load_char(file):
+    attr = []
+    with open(file, 'r') as char_file:
+        for line in char_file:
+            line = line.strip()
+            attr.append(line)
+    char = Character(attr[0],attr[1],attr[2],attr[3].attr[4])
+    return char
+
 
 spartan = Race("Spartan", 15, 10, 10, 5, 20)
 human = Race("Human", 10, 5, 5, 2)
@@ -35,3 +113,7 @@ hunter = Race("Hunter", 30, 40, 7, 2)
 
 cqc = "cqc"
 engineer = "engineer"
+sharpshooter= "sharpshooter"
+
+chief = Character("John-117", 100, spartan, cqc, air_assault)
+chief.save_char()
